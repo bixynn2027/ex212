45c22b632534e753d7c770a96b58782127c2d5f6
bixynn2027 <59707557+bixynn2027@users.noreply.github.com>
2020-01-24 01:04:09 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex212
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数値を入力");
+            int a = int.Parse(Console.ReadLine());
+            if (a % 2 == 0) {
+                Console.WriteLine("2の倍数です");
+            }
+            if (a % 3 == 0) {
+                Console.WriteLine("3の倍数です");
+            }
+            if(a % 2 == 0 && a % 3 == 0)
+            {
+                Console.WriteLine("2と3の公倍数です");
+            }
+        }
+    }
+}
